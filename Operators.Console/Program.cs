e3561de1c3ad244e67f1551fa2897c70c3c1d4ed
60int <60int@protonmail.com>
2022-09-09 22:10:38 +0200

    New example day 1

---
+++ Operators.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+class Program
+{
+    static readonly List<Expression> expressions = new();
+    static void Main()
+    {
+        ReadCSV();
+    }
+    private static void ReadCSV()
+    {
+        using StreamReader reader = new("expressions.txt");
+        while (!reader.EndOfStream)
+        {
+            expressions.Add(new Expression(reader.ReadLine()));
+        }
+        reader.Close();
+    }
+}
+class Expression
+{
+    public int FirstOperand { get; set; }
+    public string? Operator { get; set; }
+    public int SecondOperand { get; set; }
+    public Expression(string line)
+    {
+        string[] parts = line.Split(' ');
+        FirstOperand = int.Parse(parts[0]);
+        Operator = parts[1];
+        SecondOperand = int.Parse(parts[2]);
+    }
+}
