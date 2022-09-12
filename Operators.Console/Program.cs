ee1d5e54378a7dde81305d2323e811a20390265d
60int <60int@protonmail.com>
2022-09-12 20:36:39 +0200

    Small example update

--- Operators.Console/Program.cs
+++ Operators.Console/Program.cs
@@ -16,6 +16,28 @@ class Program
         }
         reader.Close();
     }
+    private static void Task1()
+    {
+        Console.WriteLine($"1. Task: {expressions.Count}");
+    }
+    private static void Task2()
+    {
+        Console.WriteLine($"2. Task: {expressions.Count(a => a.Operator == "mod")}");
+    }
+    private static void Task3()
+    {
+        Console.WriteLine($"3. Task: {(expressions.FirstOrDefault(a => a.FirstOperand % 10 == 0 && a.SecondOperand % 10 == 0) != null ? "There aren't any" : "There are")}");
+    }
+    private static void Task4()
+    {
+        Console.WriteLine("4. Task: ");
+        Console.WriteLine($"\tmod -> {expressions.Count(o => o.Operator == "mod")}");
+        Console.WriteLine($"\t/ -> {expressions.Count(o => o.Operator == "/")}");
+        Console.WriteLine($"\tdiv -> {expressions.Count(o => o.Operator == "div")}");
+        Console.WriteLine($"\t- -> {expressions.Count(o => o.Operator == "-")}");
+        Console.WriteLine($"\t* -> {expressions.Count(o => o.Operator == "*")}");
+        Console.WriteLine($"\t+ -> {expressions.Count(o => o.Operator == "+")}");
+    }
 }
 class Expression
 {
