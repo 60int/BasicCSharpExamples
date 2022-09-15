8fd2100c8056761830cb1dd623bdbad5c4bd10cf
60int <60int@protonmail.com>
2022-09-15 21:38:02 +0200

    Example tasks added

--- Pilots.Console/Program.cs
+++ Pilots.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
 
 class Program
 {
@@ -6,6 +8,11 @@ class Program
     static void Main()
     {
         ReadCSV();
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
     }
     private static void ReadCSV()
     {
@@ -15,6 +22,29 @@ class Program
             pilots.Add(pilot);
         }
     }
+    private static void Task1()
+    {
+        Console.WriteLine($"1. Task: {pilots.Count}");
+    }
+    private static void Task2()
+    {
+        Console.WriteLine($"2. Task: {pilots.Last().Name}");
+    }
+    private static void Task3()
+    {
+        Console.WriteLine("3. Task: ");
+        pilots.Where(a => a.DateOfBirth < DateTime.Parse("1901.01.01")).ToList().ForEach(x => Console.WriteLine($"\t{x.Name} ({x.DateOfBirth.ToShortDateString()})"));
+    }
+    private static void Task4()
+    {
+        Console.WriteLine($"4. Task: {pilots.FindAll(b => b.StartNumber > 0).OrderBy(a => a.StartNumber).First().Nationality}");
+    }
+    private static void Task5()
+    {
+        Console.WriteLine("5. Task: ");
+        pilots.GroupBy(a => a.StartNumber).Where(g => g.Count() > 1 && g.Key != 0).ToList().ForEach(a => Console.Write(a.Key + ", "));
+    }
+
 }
 class Pilot
 {
